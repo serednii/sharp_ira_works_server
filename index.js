e0e4dc90321d9aa3496e484113282154182163a9
Serednii Mykola <seredniimykola@gmail.com>
2024-10-18 00:40:23 +0200

    add multi server

--- index.js
+++
@@ -1,8 +0,0 @@
-const sharp = require('sharp');
-
-sharp('./input_img/2019_0002.jpg')
-    .resize(300, 200) // Зміна розміру
-    .toFile('./output_img/2019_0002.jpg', (err, info) => {
-        if (err) throw err;
-        console.log(info);
-    });
